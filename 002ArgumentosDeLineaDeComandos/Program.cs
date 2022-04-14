0877adc6a7646f9ccdcf92cb8e57abf26c27f59c
David Amador <54488581+dsamador@users.noreply.github.com>
2022-04-14 15:08:26 -0500

    Argumentos por línea de comandos

---
+++ 002ArgumentosDeLineaDeComandos/Program.cs
@@ -0,0 +1,19 @@
+﻿string nombre = "";
+int repeticiones = 0;
+int n = 0;
+
+//verificar que se tenga la cantidad de argumentos
+if (args.Length != 2)
+{
+    Console.WriteLine("Debe poner el nombre y las repeticiones");
+}
+else
+{
+    //obtener los argumentos
+    nombre = args[0];
+    repeticiones = Convert.ToInt32(args[1]);
+
+    //hacemos el saludo
+    for (int i = 0; i < repeticiones; i++)    
+        Console.WriteLine($"Hola {nombre}");
+}
