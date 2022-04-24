aea48e37a54ce4f9c2ab027b5841aa1c7dd94406
David Amador <54488581+dsamador@users.noreply.github.com>
2022-04-24 08:45:04 -0500

    Formato terminado

--- 006FormatoEnWrite y WriteLine/Program.cs
+++ 006FormatoEnWrite y WriteLine/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double miDoble = 123456.7890123;
+int miInt = 637;
+
+Console.WriteLine("Impresiones normales");
+Console.WriteLine($"El entero {miInt}");
+Console.WriteLine($"El doble {miDoble}");
+Console.WriteLine("-----------------");
+Console.WriteLine("Impresiones con formato");
+Console.WriteLine($"Moneda {miDoble:C}");
+Console.WriteLine($"Rellena lugares {miInt:D5}");
+Console.WriteLine($"Exponente {miDoble:E}");
+Console.WriteLine($"Lugares decimales {miDoble:F3}");
+Console.WriteLine($"Exponente con lugares {miDoble:G3}");
+Console.WriteLine($"Exponente con lugares {miDoble:G5}");
+Console.WriteLine($"Formato numerico {miDoble:N}");
+Console.WriteLine($"En Hexadecimal {miInt:X4}");
+
