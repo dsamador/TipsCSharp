58f0ffead5328502d58796de017bc79aecaf2573
David Amador <54488581+dsamador@users.noreply.github.com>
2022-05-01 19:38:27 -0500

    11 Uso de random

---
+++ 011 Uso de random/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Instanciar la clase
+ Obtener de diferentes formas numeros aleatorios
+ Usar tecnicas de depuracion cuando usemos random*/
+
+/*Los numeros random son numeros que se escogen
+al azar. Realmente no son tan aleatorios ya que se
+seleccionan de una tabla muy grande de numeros y 
+nos son dados como si fueran aleatorios*/
+
+/*En C# los numeros random se sacan de una clase
+llamada Random*/
+
+//variables
+Random random = new Random();
+
+int n = 0;
+int numero = 0;
+double valor = 0.0;
+byte[] bytes = new byte[3];
+
+//Numero entre cero y 2,147,483,647
+numero = random.Next();
+Console.WriteLine(numero);
+
+//Numero entre cero y menor al que coloque. El 
+//colocado debe ser > 0 y menor a 19
+numero = random.Next(20);
+Console.WriteLine(numero);
+
+//Numero entre el menor y menor al que sea mayor
+numero = random.Next(-15, 10);//-11, 0, 9
+Console.WriteLine(numero);
+
+//Numero entre 0 y 1
+valor = random.NextDouble();
+Console.WriteLine(valor);
+
+//Llena el arreglo con valores entre 0 y 255
+random.NextBytes(bytes);
+for (n = 0; n < 3; n++)
+    Console.WriteLine(bytes[n]);
+
+/*Otra manera de instanciar a random es poniendo
+una semilla o seed la cual indica la posicion
+dentro de la tabla a donde queremos que se empiecen
+a generar los numeros. Esto se hacia antes.*/
+/*En este caso la semilla es la cantidad de 
+ticks desde que se encendio el pc*/
+Random random2 = new Random((int)DateTime.Now.Ticks
+    & 0x0000FFFF);
+
+/*Si colocamos un valor fijo vamos a tener los 
+mismos numeros aleatorios, es decir ya no seran
+aleatorios.*/
+Random random3 = new Random(50);
