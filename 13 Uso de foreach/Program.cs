a4bf0580cc29dd6a5b9ab740a92cb642799aa297
David Amador <54488581+dsamador@users.noreply.github.com>
2022-05-07 12:17:36 -0500

    13 Foreach

--- 13 Uso de foreach/Program.cs
+++ 13 Uso de foreach/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Collections;
+
+ArrayList ciudades = new ArrayList();
+int[] valores = new int[4];
+
+ciudades.Add("Santa Marta");
+ciudades.Add("Guachaca");
+ciudades.Add("Cienaga");
+ciudades.Add("Utica");
+ciudades.Add("Barranquilla");
+
+valores[0] = 5;
+valores[1] = 9;
+valores[2] = 7;
+valores[3] = 10;
+
+foreach (var item in ciudades)
+    Console.WriteLine($"La ciudad es {item}");
+
+foreach (var item in valores)
+    Console.WriteLine($"El numero es {item}");
+
+
