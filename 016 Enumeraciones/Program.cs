f135a150719dc165a3852fc14ab2f2cde29ee099
David Amador <54488581+dsamador@users.noreply.github.com>
2022-05-31 14:59:53 -0500

    Enumeraciones

---
+++ 016 Enumeraciones/Program.cs
@@ -0,0 +1,34 @@
+﻿string dato = "";
+double tamanoImg = 0.0;
+
+Console.WriteLine("Dame la dimension de la imagen");
+dato = Console.ReadLine();
+tamanoImg = Convert.ToDouble(dato);
+
+calcularEspacio(tipoImagen., tamanoImg);
+
+static void calcularEspacio(tipoImagen imagen, double tamanoImg)
+{
+    double espacio = 0;
+    switch (imagen)
+    {
+        case tipoImagen.BMP:
+            espacio = tamanoImg * 1.1;
+            break;
+        case tipoImagen.JPG:
+            espacio = tamanoImg * 0.8;
+            break;
+        case tipoImagen.PNG:
+            espacio = tamanoImg * 0.77;
+            break;
+        case tipoImagen.GIF:
+            espacio = tamanoImg * 1.5;
+            break;
+        case tipoImagen.TIFF:
+            espacio = tamanoImg * 0.97;
+            break;
+    }
+
+    Console.WriteLine($"La dimension final es {espacio}");
+}
+enum tipoImagen { BMP, JPG, PNG, GIF, TIFF };
