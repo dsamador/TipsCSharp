ec0b51851212d91b4f029291548e9572745a434e
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-02 13:07:03 -0500

    Override a los Object Methods

---
+++ 018 Override a Object/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace _018_Override_a_Object
+{
+    public class Persona : Object
+    {
+        private string nombre;
+        private int edad;
+        public Persona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public override string ToString()
+        {
+            //esto no funciona
+            //return Console.WriteLine($"Nombre: {nombre}, edad: {edad}");
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat($"Nombre: {nombre}, edad: {edad}");
+            return sb.ToString();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj != null && obj is Persona)
+            {
+                Persona temp = (Persona)obj;
+                if(nombre == temp.nombre && edad == temp.edad)
+                    return true;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return nombre.GetHashCode() + edad.GetHashCode();
+        }
+    }
+}
---
+++ 018 Override a Object/Program.cs
@@ -0,0 +1,26 @@
+﻿using _018_Override_a_Object;
+
+Persona persona = new Persona("J", 3);
+
+Console.WriteLine("------ToString------");
+Console.WriteLine(persona.ToString());
+
+Console.WriteLine("------GetType------");
+Console.WriteLine(persona.GetType());
+Console.WriteLine(persona.GetType().BaseType);
+
+Persona persona2 = new Persona("J", 3);
+
+Console.WriteLine("------Equals------");
+if (persona.Equals(persona2))
+    Console.WriteLine("Son iguales");
+else
+    Console.WriteLine("No son iguales");
+
+Object persona3 = persona;
+if (persona3.Equals(persona))
+    Console.WriteLine("Son iguales");
+else
+    Console.WriteLine("No son iguales");
+
+Console.WriteLine(persona.GetHashCode());
