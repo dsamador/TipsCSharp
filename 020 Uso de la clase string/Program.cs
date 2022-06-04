3aec3b7367342add426beb5368601d8bd4045942
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-04 18:46:09 -0500

    string completo

--- 020 Uso de la clase string/Program.cs
+++ 020 Uso de la clase string/Program.cs
@@ -20,5 +20,63 @@ Console.WriteLine("-------------------");
 int indice = 0;
 indice = mensaje.IndexOf('s');
 Console.WriteLine($"La s aparece en el indice {indice}");
+
+//Encontramos el lugar donde aparece una cadena
+indice = mensaje.IndexOf("Tiene");
+Console.WriteLine($"La palabra aparece en el indice {indice}");
 Console.WriteLine("-------------------");
 
+//Encontramos el lugar donde aparece un valor por ultima vez
+indice = mensaje.LastIndexOf('a');
+Console.WriteLine($"Aparace por ultimo en el indice {indice}");
+Console.WriteLine("-------------------");
+
+//Indicamos la longitud y se llena con espacios
+//Programando tiene 11 caracteres, con el 15 sumamos 4
+//espacios a la izquierda.
+string mensaje3 = "Programando";
+string pad = mensaje3.PadLeft(5);
+Console.WriteLine(pad);
+Console.WriteLine("-------------------");
+
+//Removemos caracteres desde la posicion indicada
+string remover = mensaje.Remove(7);
+Console.WriteLine(remover);
+Console.WriteLine("-------------------");
+
+//Reemplazamos caracteres o subcadenas
+string reemplazo = mensaje.Replace('a', 'i');
+Console.WriteLine(reemplazo);
+reemplazo = mensaje.Replace("saludos", "regalos");
+Console.WriteLine(reemplazo);
+Console.WriteLine("-------------------");
+
+//Separar palabras teniendo en cuenta el espacio en blanco
+//y el punto como separadores
+string[] palabras = mensaje.Split(new char[] { ' ', '.' });
+foreach(string palabra in palabras)
+    Console.WriteLine(palabra);
+Console.WriteLine("-------------------");
+
+//Retorna bool si la cadena empieza por hola.
+bool empieza = mensaje.StartsWith("hola");
+Console.WriteLine(empieza);
+Console.WriteLine("-------------------");
+
+//Obtenemos una subcadena desde un indice hasta otro
+string subcadena = mensaje.Substring(7, 12);
+Console.WriteLine(subcadena);
+Console.WriteLine("-------------------");
+
+//Mandamos a minusculas
+string mensaje4 = "ESTE mensaje tiene Mayusculas";
+string minusculas = mensaje4.ToLower();
+string mayusculas = mensaje4.ToUpper();
+Console.WriteLine(minusculas);
+Console.WriteLine(mayusculas);
+Console.WriteLine("-------------------");
+
+//Eliminamos el exceso de caracteres TrimEnd/Start
+string limpio = mensaje.Trim(new char[] {'!'});
+Console.WriteLine(limpio);
+Console.WriteLine("-------------------");
