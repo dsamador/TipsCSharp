422586808c727efd94842b38a9432d5423f9cd7a
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-04 13:18:04 -0500

    La clase string

---
+++ 020 Uso de la clase string/Program.cs
@@ -0,0 +1,24 @@
+﻿string mensaje = "Hola, saludos a todos!!!!!!!!!!!!!!!!";
+
+//verificamos si hay una subcadena
+bool tiene = mensaje.Contains("saludos");
+Console.WriteLine(tiene);
+Console.WriteLine("-------------------");
+
+//Verificamos si termina en una subcadena en particular.
+bool termina = mensaje.EndsWith(".");
+Console.WriteLine(termina);
+Console.WriteLine("-------------------");
+
+//Creamos una cadena por medio de formato
+int edad = 20;
+string mensaje2 = String.Format("La persona tiene {0} years de edad", edad);
+Console.WriteLine(mensaje2);
+Console.WriteLine("-------------------");
+
+//Encontramos el lugar donde aparece determinado caracter por primera vez
+int indice = 0;
+indice = mensaje.IndexOf('s');
+Console.WriteLine($"La s aparece en el indice {indice}");
+Console.WriteLine("-------------------");
+
