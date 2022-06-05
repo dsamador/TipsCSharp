2e9d0f4cd983e835a245723757e74565daf71f44
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-05 06:37:51 -0500

    Operador ternario

---
+++ 023 Operador ternario/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Operador ternario
+// condicion ? V : F
+
+int a = 15;
+int b = 3;
+int r = 0;
+
+//selecciona el mayor
+r = (a > b) ? a : b;
+Console.WriteLine($"El mayor es {r}");
+
+//Llevamos a cabo una asignacion
+r = (a < 6) ? a = 10 : a = -10;
+Console.WriteLine($"a = {a}, r = {r}");
+
+//Hacemos incremento
+r = (a > 5) ? a++ : b++;
+Console.WriteLine($"a = {a}, b = {b}, r = {r}");
+
+//Hacemos invocacion a un metodo
+static int mensaje()
+{
+    Console.WriteLine("Hola");
+    return 50;
+}
+r = (a < 0) ? mensaje() : 0;
+Console.WriteLine($"r = {r}");
