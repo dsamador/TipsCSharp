0a070f50d58ca113bfc48066089a54108d4e9c44
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-06 14:22:16 -0500

    Coalescente y Null-Condicional

---
+++ 024 Operadores de Null/Program.cs
@@ -0,0 +1,21 @@
+﻿string a = null;
+string b = "hola";
+string r;
+
+r = a ?? "Fue un null";
+Console.WriteLine(r);
+
+r = b ?? "Fue un null";
+Console.WriteLine(r);
+
+Console.WriteLine("---------");
+
+Console.WriteLine("Operador Null-Condicional o Elvis");
+string rs = a.ToString();//null reference exception
+string rs2 = a?.ToString();//No da error. Pero no pone nada
+Console.WriteLine(rs2);
+
+//Para que funcione, la expresion debe de aceptar un null
+
+//int cantidad = a?.ToString().Length;
+int? cantidad = a?.ToString().Length;
