e2f4553942e4fe8802aecb0df4ebec5f70a014f1
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-07 14:50:09 -0500

    Inicializadores de objetos

---
+++ 026 Inicializadores de Objetos/Program.cs
@@ -0,0 +1,7 @@
+﻿using _026_Inicializadores_de_Objetos;
+
+Prueba p = new Prueba { a = 1, b = 2, c = "Saludos"};
+Console.WriteLine(p);
+
+Prueba p1 = new Prueba("Saludo 2") { a = 1, b = 2 };
+Console.WriteLine(p1);
---
+++ 026 Inicializadores de Objetos/Prueba.cs
@@ -0,0 +1,23 @@
+﻿namespace _026_Inicializadores_de_Objetos
+{
+    public class Prueba
+    {
+
+        public int a;
+        public int b;
+        public string c;
+
+        public Prueba()
+        {                      
+        }
+        public Prueba(string c)
+        {          
+            this.c = c;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{a}, {b}, {c}");
+        }
+    }
+}
