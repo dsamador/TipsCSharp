1cc9a83bd56b05343bbf9d1f48d6be958fc88b41
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-08 19:42:51 -0500

    Más sobre propiedades

---
+++ 027 Mas sobre propiedades/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using _027_Mas_sobre_propiedades;
+
+Prueba obj = new Prueba(50);
+
+double pv = obj.PrecioVenta;
+Console.WriteLine($"Precio venta: {pv}");
+
+double imp = obj.Impuesto;
+Console.WriteLine($"Impuesto: {imp}");
+
+obj.Descuento = 15;
+double precioDescuento = obj.Descuento;
+Console.WriteLine($"El precio con descuento: {precioDescuento}");
+
+Console.WriteLine($"Objetos en existencia {obj.Inventario}");
+
+//No es permitido porque el set es privado
+//obj.Garantia = 3;
+
+Console.WriteLine($"La garantia es por {obj.Garantia} years");
+
+obj.ponerGarantia(12345, 3);
+Console.WriteLine($"La garantia es por {obj.Garantia} years");
---
+++ 027 Mas sobre propiedades/Prueba.cs
@@ -0,0 +1,47 @@
+﻿namespace _027_Mas_sobre_propiedades
+{
+    public class Prueba
+    {
+        
+
+        //propiedad calculada
+        private double precioCompra;
+        private double precioVenta;
+        public double PrecioVenta { get { return precioCompra * 1.30; } }
+
+        public Prueba(double precioCompra)
+        {
+            this.precioCompra = precioCompra;
+        }
+
+        //Expression-bodied properties
+        //Propiedades de expresion representada
+        public double Impuesto => PrecioVenta * 0.16;
+
+
+        //Version C# 7 permite el set
+        private double descuento;
+        public double Descuento
+        {
+            get => precioCompra * (1 - descuento);
+            set => descuento = value / 100;
+        }
+
+        //Inicializador de propiedades
+        public int Inventario { get; set; } = 30;
+
+        //Accesibilidad
+        private int garantia;
+        public int Garantia
+        {
+            get => garantia;
+            private set => garantia = value;
+        }
+
+        public void ponerGarantia(int password, int garantia)
+        {
+            if (password == 12345)
+                Garantia = garantia;
+        }
+    }
+}
