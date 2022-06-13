68a0414e55647eebe51447e495683005d98045a6
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-13 17:14:03 -0500

    Operador shift

---
+++ 034 Uso de operadores shift/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = 5;
+string binario = "";
+
+binario = Convert.ToString(n, 2).PadLeft(8,'0');
+Console.WriteLine($"Bin:{binario}, Dec:{n}");
+
+//Hacemos el shift a la izquierda por 1 bit
+n = n << 1;
+binario = Convert.ToString(n, 2).PadLeft(8, '0');
+Console.WriteLine($"Bin:{binario}, Dec:{n}");
+
+//Hacemos el shift a la izquierda por 3 bit
+n = n << 3;
+binario = Convert.ToString(n, 2).PadLeft(8, '0');
+Console.WriteLine($"Bin:{binario}, Dec:{n}");
+
+//Hacemos el shift a la derecha por 1 bit
+n = n >> 3;
+binario = Convert.ToString(n, 2).PadLeft(8, '0');
+Console.WriteLine($"Bin:{binario}, Dec:{n}");
