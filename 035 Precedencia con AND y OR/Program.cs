427f02b8c0c0039c132d1e1629b551a3e18cb5fe
David Amador <54488581+dsamador@users.noreply.github.com>
2022-06-14 13:44:08 -0500

    Precedencia de AND y OR

---
+++ 035 Precedencia con AND y OR/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 0;
+int b = 0;
+
+bool r1 = false;
+bool r2 = false;
+
+for (a = 0; a < 2; a++)
+{
+    for (b = 0; b < 2; b++)
+    {
+        //r1 = (a == 0 && b == 1) || b == 0;
+        //r2 = a == 0 && b == 1 || b == 0;
+
+        r1 = (a == 0 || b == 1) && a == 0;
+        r2 = a == 0 || b == 1 && a == 0;
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write($"{a} {b}");
+
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.Write($" {r1} ");
+
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine($"{r2}");
+    }
+}
